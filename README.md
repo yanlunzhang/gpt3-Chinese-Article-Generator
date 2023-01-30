2a60172c279848e1f29204edeabd8708a9181f21
yanlunzhang <123820585+yanlunzhang@users.noreply.github.com>
2023-01-30 16:36:59 +0800

    initial release

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # buildspace 
 ### Welcome 👋
 This is the starter template for the [build your own AI writing assistant w/ GPT-3](https://buildspace.so/builds/ai-writer) project.
+
+This is a buildspace practice project.
+
+注意：点击‘干’之后没反应请耐心多等一会儿，openai的速度比较慢。
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,40 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+const [isGenerating, setIsGenerating] = useState(false)
+
+const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
@@ -11,10 +43,33 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>你只需要想个标题</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>剩下的工作由人工智能来完成</h2>
+          </div>
+          <div className="prompt-container">
+            <textarea placeholder="//告诉我你的文章是什么标题(示例：土豆和鸡蛋的故事）" className="prompt-box" 
+              value={userInput} onChange={onUserChangedText} />
+            <div className="prompt-buttons">
+              <a className="generate-button" onClick={callGenerateEndpoint}>
+              <div className="generate">
+        <p>干</p>
+      </div>
+    </a>
+  </div>
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
           </div>
         </div>
       </div>
@@ -30,8 +85,11 @@ const Home = () => {
           </div>
         </a>
       </div>
+      
     </div>
   );
 };
 
 export default Home;
+
+
